81474574f28d0fa14b347a77581b51369a480659
chris <chris@ringeval.com>
2024-05-18 17:33:10 +0200

    Fix missing bandwidth for integrated fluxes

--- wmSpaceWeather/getspaceweather.py
+++ wmSpaceWeather/getspaceweather.py
@@ -55,6 +55,7 @@ def main():
         E2m = median(E2)
     except:
         print("cannot retrieve electron fluxes!")
+        E0m = -999
         E1m = -999
         E2m = -999
               
@@ -64,6 +65,7 @@ def main():
         P1m = median(P1)
         P2m = median(P2)
         P3m = median(P3)
+
     except:
         print("cannot retrieve proton fluxes!")
         P1m = -999
@@ -77,7 +79,10 @@ def main():
     except:
         CX = 'Z0.0'
 
-              
+#    print(E0m,E1m,E2m)
+#    print(P1m,P2m,P2m)
+
+        
     dumpLatestKp(wmdates,Kps,E1m,E2m,P1m,P2m,P3m,CX)
     
 
@@ -152,6 +157,17 @@ def getElectronFluxes(t,dt):
     E9flux = []
     E10flux = []
     meastime = []
+
+    dE1 = 115-80
+    dE2 = 165-115
+    dE3 = 235-165
+    dE4 = 340-235
+    dE5 = 500-340
+    dE6 = 700-500
+    dE7 = 1000-700
+    dE8 = 1900-1000
+    dE9 = 3200-1900
+    dE10 = 6500-3200
     
     for i in range(len(result)-1,-1,-1):
         achannel = result[i]
@@ -159,25 +175,25 @@ def getElectronFluxes(t,dt):
         if (endtime - atime < dt):
             match achannel['energy']:
                 case '80-115 keV':
-                    E1flux.append(achannel['flux'])
+                    E1flux.append(dE1*achannel['flux'])
                 case '115-165 keV':
-                    E2flux.append(achannel['flux'])
+                    E2flux.append(dE2*achannel['flux'])
                 case '165-235 keV':
-                    E3flux.append(achannel['flux'])
+                    E3flux.append(dE3*achannel['flux'])
                 case '235-340 keV':
-                    E4flux.append(achannel['flux'])
+                    E4flux.append(dE4*achannel['flux'])
                 case '340-500 keV':
-                    E5flux.append(achannel['flux'])
+                    E5flux.append(dE5*achannel['flux'])
                 case '500-700 keV':
-                    E6flux.append(achannel['flux'])
+                    E6flux.append(dE6*achannel['flux'])
                 case '700-1000 keV':
-                    E7flux.append(achannel['flux'])
+                    E7flux.append(dE7*achannel['flux'])
                 case '1000-1900 keV':
-                    E8flux.append(achannel['flux'])
+                    E8flux.append(dE8*achannel['flux'])
                 case '1900-3200 keV':
-                    E9flux.append(achannel['flux'])
+                    E9flux.append(dE9*achannel['flux'])
                 case '3200-6500 keV':
-                    E10flux.append(achannel['flux'])
+                    E10flux.append(dE10*achannel['flux'])
                 case others:
                     print('energy channel missed: ',achannel['energy'])
 
@@ -195,7 +211,7 @@ def getElectronFluxes(t,dt):
 
     Elowflux = [sum(x) for x in zip(E1flux,E2flux,E3flux,E4flux,E5flux,E6flux)]
     E800KeV = [sum(x) for x in zip(E7flux,E8flux,E9flux,E10flux)]
-    E2MeV= [sum(x) for x in zip(E8flux,E9flux,E10flux)]
+    E2MeV= [sum(x) for x in zip(E9flux,E10flux)]
 
     return meastime, Elowflux, E800KeV, E2MeV
 
@@ -221,6 +237,20 @@ def getProtonFluxes(t,dt):
     P9flux = []
     P10flux = []    
     meastime = []
+
+    dP1 = 1860-1020
+    dP2A = 2300-1900
+    dP2B = 3340-2310
+    dP3 = 6480-3400
+    dP4 = 11000-5840
+    dP5 = 23270-11640
+    dP6 = 38100-25900
+    dP7 = 73400-40300
+    dP8A = 98500-83700
+    dP8B = 118000-99900
+    dP8C = 143000-115000
+    dP9 = 242000-160000
+    dP10 = 404000-276000
     
     for i in range(len(result)-1,-1,-1):
         achannel = result[i]
@@ -228,31 +258,31 @@ def getProtonFluxes(t,dt):
         if (endtime - atime < dt):
             match achannel['channel']:
                 case 'P1':
-                    P1flux.append(achannel['flux'])
+                    P1flux.append(dP1*achannel['flux'])
                 case 'P2A':
-                    P2Aflux.append(achannel['flux'])
+                    P2Aflux.append(dP2A*achannel['flux'])
                 case 'P2B':
-                    P2Bflux.append(achannel['flux'])
+                    P2Bflux.append(dP2B*achannel['flux'])
                 case 'P3':
-                    P3flux.append(achannel['flux'])
+                    P3flux.append(dP3*achannel['flux'])
                 case 'P4':
-                    P4flux.append(achannel['flux'])
+                    P4flux.append(dP4*achannel['flux'])
                 case 'P5':
-                    P5flux.append(achannel['flux'])
+                    P5flux.append(dP5*achannel['flux'])
                 case 'P6':
-                    P6flux.append(achannel['flux'])
+                    P6flux.append(dP6*achannel['flux'])
                 case 'P7':
-                    P7flux.append(achannel['flux'])
+                    P7flux.append(dP7*achannel['flux'])
                 case 'P8A':
-                    P8Aflux.append(achannel['flux'])
+                    P8Aflux.append(dP8A*achannel['flux'])
                 case 'P8B':
-                    P8Bflux.append(achannel['flux'])
+                    P8Bflux.append(dP8B*achannel['flux'])
                 case 'P8C':
-                    P8Cflux.append(achannel['flux'])
+                    P8Cflux.append(dP8C*achannel['flux'])
                 case 'P9':
-                    P9flux.append(achannel['flux'])
+                    P9flux.append(dP9*achannel['flux'])
                 case 'P10':
-                    P10flux.append(achannel['flux'])                                        
+                    P10flux.append(dP10*achannel['flux'])                                      
                 case others:
                     print('energy channel missed: ',achannel['channel'])
 
@@ -264,6 +294,7 @@ def getProtonFluxes(t,dt):
                 meastime.append(atime)
 
     # The three channels of wmspaceweather(>10MeV, >50MeV, >100MeV)
+    
     P10MeV = [sum(x) for x in zip(P5flux,P6flux,P7flux,P8Aflux,P8Bflux,P8Cflux,P9flux,P10flux)]
     P50MeV = [sum(x) for x in zip(P7flux,P8Aflux,P8Bflux,P8Cflux,P9flux,P10flux)]
     P100MeV = [sum(x) for x in zip(P8Bflux,P8Cflux,P9flux,P10flux)]
